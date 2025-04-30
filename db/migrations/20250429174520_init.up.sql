67953cd80c5a75a500a7b2ed9d87928e428d72ba
Phil Salant <PSalant@gmail.com>
2025-04-30 17:16:05 -0600

    Add an `""` check constraint to non-nullables
    SQLite differentiates between `NULL` and `""`.

--- db/migrations/20250429174520_init.up.sql
+++ db/migrations/20250429174520_init.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY,
-    isbn TEXT NOT NULL UNIQUE,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL,
+    isbn TEXT NOT NULL UNIQUE CHECK (LENGTH(TRIM(isbn)) > 0),
+    title TEXT NOT NULL CHECK (LENGTH(TRIM(title)) > 0),
+    author TEXT NOT NULL CHECK (LENGTH(TRIM(author)) > 0),
     description TEXT,
     checked_out_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -27,7 +27,7 @@ CREATE INDEX idx_books_checked_out_at ON books(checked_out_at);
 --
 CREATE TABLE IF NOT EXISTS book_events (
     id INTEGER PRIMARY KEY,
-    book_id INTEGER NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+    book_id INTEGER NOT NULL CHECK (LENGTH(TRIM(book_id)) > 0) REFERENCES books(id) ON DELETE CASCADE,
     action TEXT NOT NULL CHECK(action IN ('checkin', 'checkout')),
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
